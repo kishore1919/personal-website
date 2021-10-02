f265008a86e7b5f298c52d5cc9a5ed8a78e32109
PoolOfDeath20 <gervinfungdaxuen@gmail.com>
2021-10-02 17:06:44 +0800

    enabled register of service worker

--- frontend/src/serviceWorkerRegistration.ts
+++ frontend/src/serviceWorkerRegistration.ts
@@ -35,7 +35,7 @@ export const register = (config?: Config) => {
         }
 
         window.addEventListener('load', () => {
-            const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+            const swUrl = './service-worker.js';
 
             if (isLocalhost) {
                 // This is running on localhost. Let's check if a service worker still exists or not.
