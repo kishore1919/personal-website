a107dbb022f700b6bd65a10abbd7a703729f8e7a
kishore1919 <kishoreselvaraj19@gmail.com>
2025-06-30 17:25:02 +0530

    feat: add content for home, contact, projects, footer, and header pages

---
+++ src/web/information/content.ts
@@ -0,0 +1,65 @@
+export const homePageContent = {
+	seo: {
+		description: "I am Kishore Selvaraj. Everything you want to know about me as a software engineer, can be found here. Feel free to poke around. Every side projects deemed important/useful will be shown here. All side projects is available as repositories/organization on Github",
+		keywords: ['Personal Website'],
+		title: "Home",
+	},
+	title: "Kishore selvaraj",
+	content: [
+		"Morning! Thank you for visiting this site!",
+		"Evening! Thank you for visiting this site!",
+		"I am Kishore Selvaraj, a software engineer based in Bangalore and I build software both for fun and for a living. I am passionate about open-source software, and I build Infrastructure and websites.",
+		"I have been coding since 2021, and it all started when I wanted to make a weather app in react and then with the Django. Later, during an internship i got in to DEVOPS, I broadened my skillset to include Docker, Terraform, Linux, Git, Kubernetes began making web applications and deploying them. After some time, I began using opensource applications and started contribution to them.",
+		"Python and bash are my primary languages in software engineering, although I believe I believe I am capable of using other languages as well, aside from Python. My passion lies in the ability to work on web applications, CLI tools, Deployment and infrastructure automation. You can find my full projects list",
+		"Outside of programming, I enjoy reading interesting articles, working out, bike rides and playing video games with my friends.",
+	],
+	links: {
+		projects: "/projects",
+	},
+};
+
+export const contactPageContent = {
+	seo: {
+		description: "Contact Kishore Selvaraj",
+		keywords: ['Contact', 'Personal Website'],
+		title: "Contact",
+		url: "/contact",
+	},
+	content: [
+		"Alright, you want to contact me?",
+		"Just drop me a line! I will do my best to respond if need be",
+	],
+};
+
+export const projectsPageContent = {
+	seo: {
+		description: "I Kishore Selvaraj. Everything you want to know about me as a software engineer, can be found here. Feel free to poke around. Every side projects deemed important/useful will be shown here. All side projects is available as repositories/organization on Github",
+		keywords: ['Personal Website'],
+		title: "Projects",
+		url: "/projects",
+	},
+};
+
+export const footerContent = {
+	location: "Bangalore",
+	copyright: "2025 - Present © Kishore selvaraj",
+	licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0",
+};
+
+export const headerContent = {
+	logo: {
+		alt: "logo",
+		src: "/images/icons/icon-72x72.png",
+		href: "/",
+	},
+	homeRoute: "home",
+	links: {
+		github: "https://github.com/kishore1919",
+		linkedin: "https://www.linkedin.com/in/kishore-selvaraj-kishore",
+		instagram: "",
+		projects: "/projects",
+		contact: "/contact",
+		medium: "https://medium.com/@kishoreselvaraj1919",
+		domain: "https://kishoreselvaraj.com",
+	},
+};
