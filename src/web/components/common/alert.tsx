7af7ea4c3d607d4d5204172c212f02ec9182b69c
kishore1919 <kishoreselvaraj19@gmail.com>
2025-06-30 16:29:22 +0530

    feat: add ClickRefresh component for animated button functionality

--- src/web/components/common/alert.tsx
+++ src/web/components/common/alert.tsx
@@ -1,6 +1,6 @@
 import type { Children } from '../../type/react';
 
-import Alert from '@mui/material/Alert';
+import Alert, { type AlertColor } from '@mui/material/Alert';
 import AlertTitle from '@mui/material/AlertTitle';
 import Snackbar from '@mui/material/Snackbar';
 import React from 'react';
@@ -8,14 +8,12 @@ import React from 'react';
 type AlertProps = Readonly<
 	Children & {
 		onClose: () => void;
+		severity: AlertColor;
+		title: string;
 	}
 >;
 
-const Container = (
-	props: Readonly<{
-		children: ReturnType<typeof Alert>;
-	}>
-) => {
+const CustomAlert = (props: AlertProps) => {
 	return (
 		<Snackbar
 			anchorOrigin={{
@@ -24,53 +22,28 @@ const Container = (
 			}}
 			open
 		>
-			{props.children}
+			<Alert onClose={props.onClose} severity={props.severity}> 
+				<AlertTitle>{props.title}</AlertTitle>
+				{props.children}
+			</Alert>
 		</Snackbar>
 	);
 };
 
-const Error = (props: AlertProps) => {
-	return (
-		<Container>
-			<Alert onClose={props.onClose} severity="error">
-				<AlertTitle>Error</AlertTitle>
-				{props.children}
-			</Alert>
-		</Container>
-	);
-};
+const Error = (props: Omit<AlertProps, 'severity' | 'title'>) => (
+	<CustomAlert {...props} severity="error" title="Error" />
+);
 
-const Warning = (props: AlertProps) => {
-	return (
-		<Container>
-			<Alert onClose={props.onClose} severity="warning">
-				<AlertTitle>Warning</AlertTitle>
-				{props.children}
-			</Alert>
-		</Container>
-	);
-};
+const Warning = (props: Omit<AlertProps, 'severity' | 'title'>) => (
+	<CustomAlert {...props} severity="warning" title="Warning" />
+);
 
-const Info = (props: AlertProps) => {
-	return (
-		<Container>
-			<Alert onClose={props.onClose} severity="info">
-				<AlertTitle>Info</AlertTitle>
-				{props.children}
-			</Alert>
-		</Container>
-	);
-};
+const Info = (props: Omit<AlertProps, 'severity' | 'title'>) => (
+	<CustomAlert {...props} severity="info" title="Info" />
+);
 
-const Success = (props: AlertProps) => {
-	return (
-		<Container>
-			<Alert onClose={props.onClose} severity="success">
-				<AlertTitle>Success</AlertTitle>
-				{props.children}
-			</Alert>
-		</Container>
-	);
-};
+const Success = (props: Omit<AlertProps, 'severity' | 'title'>) => (
+	<CustomAlert {...props} severity="success" title="Success" />
+);
 
 export { Error, Warning, Info, Success };
